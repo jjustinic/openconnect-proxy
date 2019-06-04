e8c541fd709958f8e3c4bedc08b74e8a4170fcc3
James Justinic <13036935+jjustinic@users.noreply.github.com>
2019-06-04 08:57:00 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -euf -o pipefail
 
-polipo proxyAddress=0.0.0.0 proxyPort=8123 socksParentProxy=localhost:11080 &
+polipo proxyAddress=0.0.0.0 proxyPort=8123 socksParentProxy=localhost:1080 &
 
 OPENCONNECT_PASSWORD_FILE=$(mktemp)
 echo "${OPENCONNECT_PASSWORD}" > "${OPENCONNECT_PASSWORD_FILE}"
@@ -10,4 +10,4 @@ if [[ -n "${OPENCONNECT_PUSH:-}" ]]; then
     echo "${OPENCONNECT_PUSH}" >> "${OPENCONNECT_PASSWORD_FILE}"
 fi
 
-cat "${OPENCONNECT_PASSWORD_FILE}" | openconnect --script-tun --script "ocproxy -D 11080" --authgroup "${OPENCONNECT_GROUP}" -u "${OPENCONNECT_USER}" --passwd-on-stdin "${OPENCONNECT_HOST}"
+cat "${OPENCONNECT_PASSWORD_FILE}" | openconnect --script-tun --script "ocproxy -D 1080" --authgroup "${OPENCONNECT_GROUP}" -u "${OPENCONNECT_USER}" --passwd-on-stdin "${OPENCONNECT_HOST}"
