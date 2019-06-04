c4c47611c7ce7be4898cb756198ead31f270b26a
James Justinic <13036935+jjustinic@users.noreply.github.com>
2019-06-04 00:22:17 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -euf -o pipefail
 
-polipo proxyAddress=0.0.0.0 proxyPort=8123 socksParentProxy=localhost:11080 authCredentials=${PROXY_USERNAME}:${PROXY_PASSWORD} &
+polipo proxyAddress=0.0.0.0 proxyPort=8123 socksParentProxy=localhost:11080 &
 
 OPENCONNECT_PASSWORD_FILE=$(mktemp)
 echo "${OPENCONNECT_PASSWORD}" > "${OPENCONNECT_PASSWORD_FILE}"
