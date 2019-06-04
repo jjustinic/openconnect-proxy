48b7583b5e429c40c941c8001f716be12b20e96d
James Justinic <13036935+jjustinic@users.noreply.github.com>
2019-06-03 22:04:38 -0500

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -euf -o pipefail
+
+polipo proxyAddress=0.0.0.0 proxyPort=8123 socksParentProxy=localhost:11080 authCredentials=${PROXY_USERNAME}:${PROXY_PASSWORD} &
+
+OPENCONNECT_PASSWORD_FILE=$(mktemp)
+echo "${OPENCONNECT_PASSWORD}" > "${OPENCONNECT_PASSWORD_FILE}"
+if [[ -n "${OPENCONNECT_PUSH:-}" ]]; then
+    echo "${OPENCONNECT_PUSH}" >> "${OPENCONNECT_PASSWORD_FILE}"
+fi
+
+cat "${OPENCONNECT_PASSWORD_FILE}" | openconnect --script-tun --script "ocproxy -D 11080" --authgroup "${OPENCONNECT_GROUP}" -u "${OPENCONNECT_USER}" --passwd-on-stdin "${OPENCONNECT_HOST}"
